dc30d72d454f00b01b0fa413623083bd81fb9e2a
cpereira7 <cpereira7@users.noreply.github.com>
2025-02-18 19:17:59 +0000

    feat(PubSub): Added the Initial App for the Pub/Sub Sample.
    - Creates a dotnet application that work as Redis publisher and Subscriber.

---
+++ SampleStack.Redis/Program.cs
@@ -0,0 +1,56 @@
+﻿using StackExchange.Redis;
+
+namespace SampleStack.Redis
+{
+    internal static class Program
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "test-channel";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select Mode: (P)ublisher / (S)ubscriber");
+            string mode = Console.ReadLine()?.Trim().ToUpper();
+
+            if (mode == "P")
+            {
+                RunPublisher();
+            }
+            else if (mode == "S")
+            {
+                RunSubscriber();
+            }
+            else
+            {
+                Console.WriteLine("Invalid option. Exiting...");
+            }
+        }
+
+        static void RunPublisher()
+        {
+            Console.WriteLine("Writing on test-channel...");
+
+            var pubsub = connection.GetSubscriber();
+            for (int i = 0; i < 10; i++)
+            {
+            pubsub.PublishAsync(Channel, "This is a test message!!", CommandFlags.FireAndForget);
+            }
+            
+            Console.Write("Messages Successfully sent to test-channel");
+        }
+
+        static void RunSubscriber()
+        {
+            Console.WriteLine("Listening on test-channel...");
+
+            var pubsub = connection.GetSubscriber();
+            pubsub.Subscribe(Channel, (channel, message) =>
+            {
+                Console.WriteLine("Message received: " + message);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
